8dc5a8bebb041c065eaf83e5508395ff595c8de7
Philipp Pendelin <philipp@pendelin.at>
2016-03-02 11:17:33 +0100

    added decision engine solution

---
+++ DecisionEngine/DecisionMaker.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DecisionMaker.Engine;
+
+namespace DecisionMaker.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var decisionEngine = new DecisionEngine();
+
+            //var groups = Enumerable.Range(0, 1000000).Select(i => decisionEngine.MakeDecision()).GroupBy(p => p).ToDictionary(p => p.Count(), p => p.Key);
+
+            while (true)
+            {
+                //System.Console.WriteLine(decisionEngine.MakeDecision());
+
+                System.Console.WriteLine(decisionEngine.MakeDecision(new[] { "1", "2" }));
+
+                System.Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ DecisionEngine/DecisionMaker.Engine/DecisionEngine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DecisionMaker.Engine
+{
+    public class DecisionEngine
+    {
+        private List<string> decisions = new List<string>
+        {
+            "Ja",
+            "Nein",
+            "Klar",
+            "Sicher nicht",
+            "Sicherlich",
+            "Frag Mutti",
+            "Jein",
+        };
+
+        /// <summary>
+        /// Make a decision based on a predefined set of possible outcomes.
+        /// </summary>
+        /// <returns>The decision.</returns>
+        public string MakeDecision()
+        {
+            return this.decisions.OrderBy(d => Guid.NewGuid()).First();
+        }
+
+        /// <summary>
+        /// Make a decision based on a given set of possible outcomes.
+        /// </summary>
+        /// <param name="decisions">Possible decisions to make.</param>
+        /// <returns>The decision.</returns>
+        public string MakeDecision(IEnumerable<string> decisions)
+        {
+            return decisions.OrderBy(d => Guid.NewGuid()).First();
+        }
+    }
+}
