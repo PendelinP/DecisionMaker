cc8b741cffdfa69e21bd4fe420d22e3f7db658da
Philipp Pendelin <philipp@pendelin.at>
2016-03-02 11:47:52 +0100

    DecisionMaker Web App and API App

---
+++ DecisionAppServices/DecisionMaker.Api/Controllers/DecisionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DecisionMaker.Engine;
+
+namespace DecisionMaker.Api.Controllers
+{
+    public class DecisionController : ApiController
+    {
+        public string Get()
+        {
+            return new DecisionEngine().MakeDecision();
+        }
+    }
+}
---
+++ DecisionAppServices/DecisionMaker.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DecisionMaker.Web.MvcApplication" Language="C#" %>
